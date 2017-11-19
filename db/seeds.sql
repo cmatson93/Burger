50f80c23d8b42d7ddd06fd56157b54771265c710
Christina  Matson <christinamatson@christinas-mbp.attlocal.net>
2017-11-18 17:11:37 -0800

    Initial Updates

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Chipotle Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('BBQ Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Jalapeno Burger', false);
